8811ed29b18613fde9793c19ad073bbf6f6ef480
Max Lechner <mkonlech@gmail.com>
2020-12-08 19:23:48 -0800

    update unit classes to pass unit tests. Include beginning inquirer role.

--- app.js
+++ app.js
@@ -14,15 +14,70 @@ const render = require("./lib/htmlRenderer");
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
 
+confirmNewMember() {
+    inquirer
+      .prompt([
+        {
+          type: "confirm",
+          name: "new",
+          message: "Do you want to add a new team member?"
+        }
+      ])
+      .then(val => {
+        // If the user says yes to a new team member take in the team member information
+        if (val.new) {
+          this.getRole();
+        } else {
+          this.render();
+        }
+      });
+  }
+
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
 // generate and return a block of HTML including templated divs for each employee!
+
+getRole() {
+
+    
+
+}
+
+memberBuild() {
+    return inquirer
+    .prompt([
+      {
+        type: "list",
+        name: "choice",
+        message: "What is your member's role?",
+        // The users guess must be a number or letter         
+      }
+    ])
+    .then(val => {
+      // If the user's guess is in the current word, log that they chose correctly
+      const didGuessCorrectly = this.currentWord.guessLetter(val.choice);
+      if (didGuessCorrectly) {
+        console.log(chalk.green("\nCORRECT!!!\n"));
+
+        // Otherwise decrement `guessesLeft`, and let the user know how many guesses they have left
+      } else {
+        this.guessesLeft--;
+        console.log(chalk.red("\nINCORRECT!!!\n"));
+        console.log(this.guessesLeft + " guesses remaining!!!\n");
+      }
+    });
+}
+
+}
+
 
 // After you have your html, you're now ready to create an HTML file using the HTML
 // returned from the `render` function. Now write it to a file named `team.html` in the
 // `output` folder. You can use the variable `outputPath` above target this location.
 // Hint: you may need to check if the `output` folder exists and create it if it
 // does not.
+
+
 
 // HINT: each employee type (manager, engineer, or intern) has slightly different
 // information; write your code to ask different questions via inquirer depending on
